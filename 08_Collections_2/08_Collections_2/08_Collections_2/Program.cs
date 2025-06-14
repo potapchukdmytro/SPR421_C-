b3cccb076a0b1821986dd3a6b4b461c4d092dd56
potapchukdmytro <traiget@gmail.com>
2025-06-14 09:22:43 +0300

    08 collections 2

---
+++ 08_Collections_2/08_Collections_2/08_Collections_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace _08_Collections_2
+{
+    internal class Program
+    {
+        static void PrintCollection(IEnumerable collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // LinkedList
+
+            //LinkedList<string> colors = new LinkedList<string>();
+
+            //colors.AddLast("red");
+            //colors.AddFirst("green");
+
+            //LinkedListNode<string> node = new LinkedListNode<string>("blue");
+            //colors.AddLast(node);
+
+            //var redNode = colors.Find("red");
+
+            //if(redNode != null)
+            //{
+            //    colors.AddAfter(redNode, "yellow");
+            //}
+
+            //PrintCollection(colors);
+
+
+
+            // HashSet
+            HashSet<int> ints = new HashSet<int>();
+            ints.Add(1);
+            ints.Add(6);
+            ints.Add(10);
+            ints.Add(6);
+            ints.Add(5);
+            ints.Add(1);
+            ints.Add(10);
+            ints.Add(2);
+            ints.Add(9);
+
+            PrintCollection(ints);
+        }
+    }
+}
